24d4202c94a7a77931f1d0be533bc4f62376d6f3
gracelcai <grace.cai7@gmail.com>
2022-01-01 08:49:04 -0800

    first commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+def operate_two(a, b):
+    if a * b == 24 or a + b == 24 or abs(a - b) == 24 or a / b == 24 or b / a == 24:
+        return True
+    else :
+        return False
+def operate_four(a, b, c, d):
+
+def find_24(a, b, c, d):
+    
+def poss_two(a, b):
+    return [a * b, a + b, abs(a - b), a / b, b / a]
+
+
+print(operate_two(2, 12))
